c321a66da52896d7cc4d1b95f7bacef1a2198283
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2023-09-24 18:41:20 -0300

    Initial commit

---
+++ ex000/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First</title>
+</head>
+<body>
+    <h1>
+    <?php 
+        echo "Hello world!";
+    
+    ?>
+    </h1>
+    <p>Me livrando aiaiai</p>
+    
+</body>
+</html>
