30e06ae18c20888e11d76fd9c827d0639e098615
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2023-09-25 20:42:55 -0300

    oi

--- ex003/index.php
+++ ex003/index.php
@@ -3,13 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>Tipos primitivos</title>
 </head>
 <body>
-    <h1>Exemplo de PHP</h1>
+
+<h1>Tipos primitivos</h1>
+
     <?php 
-        echo "Hoje é dia " . date("D/M/Y");
-        echo "e a hora atual é " . date("G:i:s");
+        $num = 0x1A;
+        echo "O valor é $num";
+        
+
+
     ?>
     
 </body>
