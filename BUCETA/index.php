e03808a9f4013b107eb4be81e2b3023274fe89c3
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2024-04-15 18:06:48 -0300

    j

--- BUCETA/index.php
+++ BUCETA/index.php
@@ -1,41 +1,77 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login AutoChoice</title>
 </head>
+
 <body>
     <?php
     require 'funcoes.php';
-    
+
     ?>
 
     <?php
-    
-    $e = $_POST['email'] ?? null;
-    $s = $_POST['senha'] ?? null;
-    $n = $_POST['nome'] ?? null;
 
-    if($e == null || $s == null){
+    $email = $_POST['email'] ?? null;
+    $senha = $_POST['senha'] ?? null;
+    $nome = $_POST['nome'] ?? null;
+
+    if ($email == null || $senha == null) {
         require 'login.php';
     } else {
-        if($n != null){
-            $query = "insert INTO usuario (nome_usuario, email, senha, tipo) VALUES ('$n', '$e', '$s', 'normal');";
+        if ($nome != null) {
 
-            $banco->query($query);
-            #era bom fazer uma validação aqui
-            echo "<h1>Cadastrado com sucesso</h1>";
+            $consulta = $banco->query("Select id_usuario, nome_usuario, email, senha, tipo from usuario where nome_usuario = '$nome'");
+
+            if($consulta != null){
+                echo "Usuário ja cadastrado.";
+                require 'login.php';
+
+            } else{
+
             
-            
 
+                $query = "insert INTO usuario (nome_usuario, email, senha, tipo) VALUES ('$nome', '$email', '$senha', 'normal');";
 
-            require 'login.php';
+                $banco->query($query);
+                #era bom fazer uma validação aqui
+                echo "<h1>Cadastrado com sucesso</h1>";
+                require 'login.php';
+            }
         }
-        echo '<h1>LOGAAAAAAADO</h1>';
 
+
+
+        $q = "Select id_usuario, nome_usuario, email, senha, tipo from usuario where email = '$em'";
+        $busca = $banco->query($q);
+
+        if (!$busca) {
+            echo 'Erro ao acessar o banco';
+        } else {
+            $objAtual = $busca->fetch_object();
+
+
+            if ($objAtual == null) {
+
+                echo "Usuário não encontrado";
+            } else {
+                if (!($sen === $objAtual->senha)) {
+                    echo 'Senha errada';
+                } else {
+                    $_SESSION['usuario'] = $objAtual->usuario;
+                    $_SESSION['nome'] = $objAtual->nome;
+                    $_SESSION['email'] = $objAtual->email;
+                    $_SESSION['tipo'] = $objAtual->tipo;
+                    echo 'Logado com sucesso';
+                    echo "<p><a href='home.php'>Clique aqui para acessar a home</a></p>";
+                }
+            }
+        }
     }
-    
+
     ?>
 
 
@@ -44,6 +80,7 @@
 
 
 
-    
+
 </body>
+
 </html>
